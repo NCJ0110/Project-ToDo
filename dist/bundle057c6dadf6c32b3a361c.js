0f9dca8e2bc632ac5e535ebec1a6cb9d9c42dd46
Nick <ncj0110@protonmail.com>
2023-05-09 02:15:08 +0100

    create header html and styles

--- dist/bundle057c6dadf6c32b3a361c.js
+++
@@ -1,9 +0,0 @@
-/******/ (() => { // webpackBootstrap
-var __webpack_exports__ = {};
-/*!**********************!*\
-  !*** ./src/index.js ***!
-  \**********************/
-console.log("testfdfs");
-/******/ })()
-;
-//# sourceMappingURL=bundle057c6dadf6c32b3a361c.js.map
--- src/index.js
+++ src/index.js
@@ -1,1 +1,2 @@
-console.log("testfdfs");
+import './styles/styles.css'
+
